306130921b370a0c87e0d6ac840818c1d502c7cd
Jacob Augunas <ragnerok@gmail.com>
2014-06-24 12:51:38 -0700

    Finished updating-users functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params)  # Not the final implementation!
+  	@user = User.new(user_params) 
   	if @user.save
       sign_in @user
   		flash[:success] = "Welcome to the Sample App"
@@ -17,6 +17,20 @@ class UsersController < ApplicationController
   	else
   		render 'new'
   	end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private 
